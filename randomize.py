a995e08281d563f3eb360184ab12bfe51a553b95
Username123467812 <erbrasel@madison.k12.wi.us>
2025-03-13 16:58:50 +0000

    added some stuff but now error needs to be fixed

--- randomize.py
+++ randomize.py
@@ -2,6 +2,11 @@ input_seed = 0
 output_seed = 0
 
 def randomize():
-    file = open("random-data-set.txt")
-    print(file)
-    file.read()
+    file = open("random-data-set.txt","r")
+    print(file.read())
+    input_seed = int(file.read())
+    output_seed = input_seed^input_seed.bit_length
+    
+    return output_seed
+
+print(randomize())
