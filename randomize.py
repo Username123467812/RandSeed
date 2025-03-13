e06f589aeda2b0bc6b31cc6711d63c2b1bbd3f8e
Username123467812 <erbrasel@madison.k12.wi.us>
2025-03-13 18:31:27 +0000

    c

--- randomize.py
+++ randomize.py
@@ -2,10 +2,13 @@ input_seed = 0
 output_seed = 0
 
 def randomize():
-    file = open("random-data-set.txt","r")
-    print(file.read())
-    input_seed = int(file.read())
-    output_seed = input_seed^input_seed.bit_length
+    # Figure out file lodaing later
+    #file = open("random-data-set.txt","r")
+    #print(file.read())
+    #input_seed = int(''.join(file.readlines()))
+    input_seed = 745612673567489501234019283
+    output_seed = input_seed**input_seed.bit_length()
+    output_seed /= int(str(output_seed).count('0'))
     
     return output_seed
 
