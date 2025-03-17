1c188dcf7c97e58887a9c24bd9ce9b50ec51bace
Username123467812 <erbrasel@madison.k12.wi.us>
2025-03-17 16:47:25 +0000

    commit i think

--- randomize.py
+++ randomize.py
@@ -2,13 +2,13 @@ input_seed = 0
 output_seed = 0
 
 def randomize():
-    # Figure out file lodaing later
+    # Figure out file loading later, having trouble converting to string
     #file = open("random-data-set.txt","r")
     #print(file.read())
     #input_seed = int(''.join(file.readlines()))
     input_seed = 745612673567489501234019283
     output_seed = input_seed**input_seed.bit_length()
-    output_seed /= int(str(output_seed).count('0'))
+    output_seed = output_seed // int(str(output_seed).count('0'))
     
     return output_seed
 
