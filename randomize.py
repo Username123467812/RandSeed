5baea78cd0330156661fc7c81784ce814a3c741b
Username123467812 <erbrasel@madison.k12.wi.us>
2025-03-18 17:55:18 +0000

    commit thing

--- randomize.py
+++ randomize.py
@@ -1,3 +1,7 @@
+import time
+import math
+from decimal import Decimal
+
 input_seed = 0
 output_seed = 0
 
@@ -9,6 +13,9 @@ def randomize():
     input_seed = 745612673567489501234019283
     output_seed = input_seed**input_seed.bit_length()
     output_seed = output_seed // int(str(output_seed).count('0'))
+    #cut output_seed down a little
+    output_seed = math.ceil(output_seed // (input_seed ** 50))
+    output_seed = output_seed * (math.ceil(time.time()) + input_seed)
     
     return output_seed
 
